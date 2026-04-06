7baaba128ce6986ef2d79d3d1dfa685503c0dc5a
Gabriel Delhay <gabrieldelhay@MBP-de-Gabriel.homenet.telecomitalia.it>
2026-04-06 22:10:57 +0200

    ex 4 not finished

--- assignment3/runExercise0_TBM.m
+++ assignment3/runExercise0_TBM.m
@@ -32,57 +32,6 @@ weights=(1/numberAssets)*ones(numberAssets,1);
 [tSelected, returnsSelected] = returnsOfInterest(inputFile, refDate, timeWindow, sharesList, formatDate);
 returnsSelected;
 
-
-%% Compute Risk Measurements
-
-% Analytic VaR & ES (Normal)
-% It computes mean and VarCovar on the proper time scale
-portfolioValue = 1e7;
-riskMeasureTimeIntervalInDays = 1;
-try
-    [ES, VaR] = AnalyticNormalMeasures(alpha, weights, portfolioValue, riskMeasureTimeIntervalInDays, returnsSelected); 
-catch err
-    err.message
-end
-
-
-%% Historical Simulation
-% TBC
-
-%% Plausibility Check
-% TBC
-
-%% Es2 
-inputFile = 'sx5e_historical_data.xls';
-refDate = datenum('15 Feb 08');   %we start taking values from here
-valuationDate = datenum('15 Feb 10');  
-NumberOfYears=2;
-timeWindow = 12*NumberOfYears;
-shares=cellstr('Generali'); 
-formatDate = 'dd/mm/yyyy';   %modified
-[tSelected, returnsSelected] = returnsOfInterest(inputFile, refDate, timeWindow, shares, formatDate); %get returns  
-costOfShares = 1164000;
-[shareData.num,shareData.cell]=xlsread(inputFile,'Data','a5:cx1295');
-[values_G, dates_G] = findSeries(shareData,underlyingCode('Generali'), formatDate);
-idx = find(dates_G <= valuationDate, 1, 'last');
-stockPrice = values_G(idx);
-numberOfShares = costOfShares / stockPrice;
-numberOfPuts = numberOfShares;
-expiry = datenum('18 Apr 10');
-strike = 28.5;
-volatility = 0.223;   %yearly
-dividendYield = 0.051;    %yearly
-alpha = 0.99;
-TTMinYears = (expiry - valuationDate) / 365;
-riskMeasureTimeIntervalInDays = 1;
-[datesSet, ratesSet] = readExcelData('MktData_CurveBootstrap.xls', formatDate);
-[dates, discounts, zeroRates]=bootstrap(datesSet, ratesSet);
-idx = find(dates <= valuationDate, 1, 'last');
-rate = zeroRates(idx);
-VaR_FullMonteCarlo = FullMonteCarloVaR(alpha, numberOfShares, numberOfPuts, stockPrice, strike, rate, dividendYield, volatility, TTMinYears, riskMeasureTimeIntervalInDays, returnsSelected); 
-VaR_DeltaNormal = DeltaNormalVaR(alpha, numberOfShares, numberOfPuts, stockPrice, strike, rate, dividendYield, volatility, TTMinYears, riskMeasureTimeIntervalInDays, returnsSelected);
->>>>>>> d16b333 (ex 4 to continue)
-
 %% Compute Risk Measurements
 
 % Analytic VaR & ES (Normal)
